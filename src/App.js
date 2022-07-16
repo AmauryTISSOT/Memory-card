f2a840ef012036c8cc012923a9f93bb6b47559fe
amaury <tissot.amaury@gmail.com>
2022-07-16 21:02:24 +0200

    Add score & best score hook to App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import Header from './components/Header';
 import Card from './components/Card'
+import { useState } from 'react';
 
 function App() {
+  
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0)
+
+
+
   return (
+
     <div className="App">
      <Header content = {'Memory Card Game'}/>
-     <Card/>
+     <Card
+     score = {score}
+     setScore = {setScore}
+     bestScore = {bestScore}
+     setBestScore = {setBestScore}
+     />
     </div>
   );
 }
